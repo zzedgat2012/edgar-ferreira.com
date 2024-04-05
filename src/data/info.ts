f6bdb8e82705468a233812252a5eb5a211815596
Edgar Ferreira <efsn88@gmail.com>
2024-04-05 07:20:29 -0300

    First commit

---
+++ src/data/info.ts
@@ -0,0 +1,99 @@
+export const info = {
+  baseUrl: "https://edgar-ferreira.com",
+  name: "Edgar Ferreira",
+  jobDescription: "DevOps Engineer",
+  about: `I'm a software engineer based in Aracaju-SE, Brazil.
+   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
+   I have been working for 8 year, with a demonstrated history of working in the Computer Software industry. Skilled in Network Architecture.
+   I have 8 years of experience in Azure(Functions, Clusters, Azure DevOps, Hybrid Enviroments), AWS, Hyper-V, Cisco Router, Mikrotik, PFSense, Windows Server. Hence,
+   I have successfully implemented several networks from scratch. 
+   Yet, I learn continuously and never stop exploring new technologies.
+   So, I have also gained knowledge of Proxmox, Oracle Cloud (OCI), OpenShift, Zabix, Grafana, Sentry, Netdata with time.
+  `,
+
+  experience: [
+    {
+      name: "Serigy Ltda",
+      location: "Aracaju - SE, Brazil",
+      startDate: "Nov 2023",
+      endDate: "Present",
+      description: [
+        "- Cloud Architect",
+        "- Developing, manage and support networks for the developer team.",
+        "- Fixing problems with clusters and implement site to site and mesh networks on Kubernetes"
+      ],
+    },
+
+    {
+      name: "WWSoftware",
+      location: "Aracaju - SE, Brazil",
+      startDate: "Set 2023",
+      endDate: "Nov 2023",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+
+    {
+      name: "Beelog Ltda",
+      location: "Aracaju - SE, Brazil",
+      startDate: "Jun 2022",
+      endDate: "Set 2023",
+      description: [
+        "- Cloud Architect",
+        "- Developing, manage and support networks for the developer team.",
+        "- Developing code in accordance with appropriate design and architectural rules",
+        "- Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "- Code Improvements and Refactoring ",
+       "- Others tasks connected to the nature of the job",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Computer Science Degree: Faculdade Descomplica",
+      location: "Rio de Janeiro",
+      startDate: "2021",
+      endDate: "2025",
+      description: [
+        "Diploma of Computer Scientist,  Computer Science Technology (CST)",
+      ],
+    },
+    {
+      name: "OCI Foundations Associate",
+      location: "Aracaju - SE, Brazil",
+      startDate: "2021",
+      endDate: "2021",
+      description: ["OCI Foundation capable professional."],
+    },
+    {
+      name: "C# Foundations",
+      location: "Aracaju - SE, Brazil",
+      startDate: "2023",
+      endDate: "2023",
+      description: ["C# Developer capable professional."],
+    },
+  ],
+
+  socialMedia: {
+    email: "mailto:dev@edgar-ferreira.com",
+    linkedin: "https://www.linkedin.com/in/efsn88/",
+    twitter: "https://twitter.com/guy_thew"
+  },
+
+  projects: [
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
